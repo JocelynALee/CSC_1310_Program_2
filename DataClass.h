d1e7a4f3aa572048828bac6fc8d32a6722bd263e
Jocelyn Lee <jalee45@tntech.edu>
2024-10-21 15:13:29 -0500

    Update DataClass.h

--- DataClass.h
+++ DataClass.h
@@ -1,1 +1,17 @@
+/********************************************************************************
+	Title: 	  dataClass.h
+	Authors:  Jocelyn Lee & Kylie Truong
+	Date:  	  11/01/2024
+	Purpose:  Header file for dataClass.cpp 
+*********************************************************************************/
+#ifndef DATACLASS_H
+#define DATACLASS_H
 
+#include "LinkedListDriver.h"
+#include "dataClass.h"
+#include "otherClass.h"
+
+using namespace std;
+
+
+#endif
