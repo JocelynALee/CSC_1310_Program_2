5c0bffffa4de390dca1541613e46c357d4eacf6d
VesperaVela <kntruong43@tntech.edu>
2024-10-21 15:34:23 -0500

    Update DataClass.cpp

--- DataClass.cpp
+++ DataClass.cpp
@@ -1,1 +1,6 @@
-
+/*************************************************************************
+	Title: 	 DataClass.cpp
+	Authors: Jocelyn Lee & Kylie Truong
+	Date:  	 11/1/2024
+	Purpose: Stores the information of superheroes, ___________________
+*************************************************************************/
