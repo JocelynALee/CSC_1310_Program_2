7cf45d7d121debc8d517bd719049a5b1fb3e7943
Jocelyn Lee <jalee45@tntech.edu>
2024-11-01 19:48:58 -0500

    Update DataClass.cpp

--- DataClass.cpp
+++ DataClass.cpp
@@ -13,47 +13,48 @@
 //****************************************************************************
 //                                 menuDisplay()
 //
-// task:          Display the user menu.
+// task:                       Display the user menu.
 //
 //****************************************************************************
 
 void menuDisplay()
 {
-	cout << "What Would You Like to Do?" << endl
-     	cout << "1. Display the best superHero Catalogue" << endl;
-	cout << "2. Display the worst superHero Catalogue" << endl;
+    do{
+	cout << "\n\nWhat Would You Like to Do?" << endl;
+    cout << "1. Display the best superHero Catalogue!!" << endl;
+	cout << "2. Display the worst superHero Catalogue!" << endl;
 	cout << "3. Add to Catalogue" << endl;
 	cout << "4. Remove from Cataloge" << endl;
 	cout << "5. Exit" << endl;
-	cin >> userInput;
-	
-	switch(userInput){
-		//Displays Superhero Catalogue
-		case 1:
-		    printSuperHero();
-		    break;
-		//Adds New Hero to Catalogue
-		case 2:
-		    addSuperhero();
-		    break;
-		//deletes hero
-		case 3:
-		    int heroDelete;
-	            printSuperHero();
-	            cout << "Which hero entry would you like to delete?" << endl;
-	        	cin >> heroDelete;
-	            removeSuperHero(index, heroDelete);
-		    break;
-		//Ends Program
-		case 4:
-		    endProgram = 1;
-		    break;
-	    }
-	    }while (endProgram != 1);
-	
-	    cout << "Up, Up, and Away!" << endl;
-	    cout << endl;
-	    
-	    return 0;
-	
-	}
+	    cin >> userInput;
+        switch(userInput)
+        {
+            //Displays Best Superhero Catalogue
+            case 1:
+                displayTopList();
+                break;
+            //Displays Worst Superhero Catalogue
+            case 2:
+                displayBottomList();
+                break;
+            //Adds New Hero to Catalogue
+            case 3:
+                addSuperhero();
+                break;
+            //deletes hero
+            case 4:
+                int heroDelete;
+                printSuperHero();
+                cout << "Which hero entry would you like to delete?" << endl;
+                cin >> heroDelete;
+                removeSuperHero(index, heroDelete);
+                break;
+            //Ends Program
+            case 5:
+                endProgram = 1;
+                break;
+        }
+	}while (endProgram != 1);
+    cout << "Up, Up, and Away!" << endl << endl;
+    return 0;
+}
