b3c797274c03075a7b3b221b903a79ebfc904c7f
Jocelyn Lee <jalee45@tntech.edu>
2024-11-02 12:37:29 -0500

    Update DataClass.h

--- DataClass.h
+++ DataClass.h
@@ -4,14 +4,42 @@
 	Date:  	  11/01/2024
 	Purpose:  Header file for dataClass.cpp 
 *********************************************************************************/
-#ifndef DATACLASS_H
-#define DATACLASS_H
-
-#include "LinkedListDriver.h"
-#include "dataClass.h"
-#include "otherClass.h"
-
+#include <string>
+#include <ostream>
 using namespace std;
 
+/*********************************************/
+/*              DataClass Class              */
+/*********************************************/
+
+class DataClass 
+{
+private:
+    string name;
+    int powerLevel;
+    string superpower;
+
+public:
+    /* Constructor */
+    DataClass(string n, int p, string s);
+
+    /* Setters */
+    void setName(const string& n);
+    void setPowerLevel(int p);
+    void setSuperPower(const string& s);
+
+    /* Getters */
+    string getName() const;
+    int getPowerLevel() const;
+    string getSuperPower() const;
+
+    /* Operators */
+    bool operator<(const DataClass& other) const;
+    bool operator>(const DataClass& other) const;
+    bool operator==(const DataClass& other) const;
+
+    /* Friend function for output */
+    friend std::ostream& operator<<(ostream& os, const DataClass& data);
+};
 
 #endif
