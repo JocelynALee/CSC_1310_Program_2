8bed406d74ae7d44f4335099a0b56b13d5df1270
Jocelyn Lee <jalee45@tntech.edu>
2024-11-02 15:04:16 -0500

    Update SmartPointer.h

--- SmartPointer.h
+++ SmartPointer.h
@@ -1,1 +1,37 @@
+/********************************************************************************
+	Title: 	  SmartPointer.h
+	Authors:  Jocelyn Lee & Kylie Truong
+	Date:  	  11/01/2024
+	Purpose:  Header file for SmartPointer.cpp 
+*********************************************************************************/
+#ifndef SMARTPOINTER_H
+#define SMARTPOINTER_H
+using namespace std;
 
+/*********************************************/
+/*           SmartPointer Class              */
+/*********************************************/
+template<typename T>
+class SmartPointer {
+	private:
+		T* ptr; 
+
+public:
+    /*Constructor*/
+    explicit SmartPointer(T* p = nullptr); 
+
+    /*Copy Constructor*/
+    SmartPointer(const SmartPointer& sp);
+
+    /*Destructor*/
+    ~SmartPointer();
+
+	/*Operators*/
+    SmartPointer& operator=(const SmartPointer& sp);
+    T& operator*() const;
+    T* operator->() const;
+    bool operator==(const SmartPointer& sp) const;
+    bool operator!=(const SmartPointer& sp) const;
+};
+
+#endif
